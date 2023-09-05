515caa2f1ea788ee55cc21435cffd5d21b825ef1
Jerry U <jerry@lyshnia.com>
2023-09-05 11:34:10 +0300

    Initial Commit

---
+++ src/test/java/com/jerry/epassi/EpassiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jerry.epassi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpassiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
